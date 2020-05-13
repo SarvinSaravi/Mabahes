e1fd17dbfce65734496d3bfe08a65bb99412ba63
SarvinSaravi <sarvin2000@gmail.com>
2020-05-13 15:55:44 +0430

    Sarvin Chenges

--- P1/Image_Enhancement.py
+++ P1/Image_Enhancement.py
@@ -1,17 +1,17 @@
 import os
-from skimage import io
+from skimage import io, color
 import matplotlib.pyplot as plt
 from skimage.transform import rescale, resize, downscale_local_mean
 
 filename_list = os.listdir('pictures')
-print(filename_list)
+# print(filename_list)
 list_of_images = []
 
 for item in filename_list:
     img = io.imread('pictures\\' + item)
     list_of_images.append(img)
 
-print(len(list_of_images))
+# print(len(list_of_images))
 
 img1 = list_of_images[0]
 
@@ -20,7 +20,18 @@ print(img1.shape)
 
 print('****************************')
 
+img1 = color.rgb2gray(img1)
 image_rescaled = rescale(img1, 0.7, anti_aliasing=False)
+
+print(type(img1))
+print(img1.shape)
+
+plt.imshow(img1)
+plt.show()
+
+plt.imshow(image_rescaled)
+plt.show()
+
 
 # print(image_rescaled)
 
