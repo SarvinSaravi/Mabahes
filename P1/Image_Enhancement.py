2458bbfb324e3fbbdde6ee7b87162b304c841d7f
SarvinSaravi <sarvin2000@gmail.com>
2020-05-13 16:37:08 +0430

    Sarvin Changes saving rescale pictures

--- P1/Image_Enhancement.py
+++ P1/Image_Enhancement.py
@@ -10,15 +10,27 @@ for item in filename_list:
     img = io.imread('pictures\\' + item)
     list_of_images.append(img)
 
+name = 1
+
 for pic in list_of_images:
     img1 = pic
 
     img1 = color.rgb2gray(img1)
     image_rescaled = rescale(img1, 0.7, anti_aliasing=False)
 
-    plt.imshow(img1)
-    plt.show()
-
     plt.imshow(image_rescaled)
-    plt.show()
+    plt.savefig(r'scaling/' + str(name) + '.png')
+
+    # my_path = os.path.abspath(__file__)
+    # my_file = str(name) + '.png'
+
+    # plt.savefig(os.path.join(my_path, my_file))
+
+    name = name + 1
+
+    # plt.imshow(img1)
+    # plt.show()
+    #
+    # plt.imshow(image_rescaled)
+    # plt.show()
 
