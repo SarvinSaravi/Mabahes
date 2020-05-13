ddf5c3d1fd40753bfdcd5fae3a50518db8b58689
SarvinSaravi <sarvin2000@gmail.com>
2020-05-13 16:09:22 +0430

    Sarvin Changes showing rescale pictures

--- P1/Image_Enhancement.py
+++ P1/Image_Enhancement.py
@@ -1,38 +1,24 @@
 import os
 from skimage import io, color
 import matplotlib.pyplot as plt
-from skimage.transform import rescale, resize, downscale_local_mean
+from skimage.transform import rescale
 
 filename_list = os.listdir('pictures')
-# print(filename_list)
 list_of_images = []
 
 for item in filename_list:
     img = io.imread('pictures\\' + item)
     list_of_images.append(img)
 
-# print(len(list_of_images))
-
 for pic in list_of_images:
     img1 = pic
-    # print(type(img1))
-    # print(img1.shape)
-
-    # print('****************************')
 
     img1 = color.rgb2gray(img1)
     image_rescaled = rescale(img1, 0.7, anti_aliasing=False)
-
-    # print(type(img1))
-    # print(img1.shape)
 
     plt.imshow(img1)
     plt.show()
 
     plt.imshow(image_rescaled)
     plt.show()
-
-
-# print(image_rescaled)
-
 
