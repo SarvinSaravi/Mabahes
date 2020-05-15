6195d0b36046f22693725e1e639680ebd715fd91
SarvinSaravi <sarvin2000@gmail.com>
2020-05-15 22:34:08 +0430

    S change -> contrast -> v2 modular version

--- P1/IE_contrast.py
+++ P1/IE_contrast.py
@@ -37,45 +37,54 @@ def plot_img_and_hist(image, axes, bins=256):
     return ax_img, ax_hist, ax_cdf
 
 
-# Load an example image
-num = 10
+def main(image):
+    img = color.rgb2gray(image)
 
-img = io.imread('pictures\\' + str(num) + '.png')
-img = color.rgb2gray(img)
+    # Gamma
+    gamma_corrected = exposure.adjust_gamma(img, 2)
 
-# Gamma
-gamma_corrected = exposure.adjust_gamma(img, 2)
+    # Logarithmic
+    logarithmic_corrected = exposure.adjust_log(img, 1)
 
-# Logarithmic
-logarithmic_corrected = exposure.adjust_log(img, 1)
+    # Display results
+    fig = plt.figure(figsize=(8, 5))
+    axes = np.zeros((2, 3), dtype=np.object)
+    axes[0, 0] = plt.subplot(2, 3, 1)
+    axes[0, 1] = plt.subplot(2, 3, 2, sharex=axes[0, 0], sharey=axes[0, 0])
+    axes[0, 2] = plt.subplot(2, 3, 3, sharex=axes[0, 0], sharey=axes[0, 0])
+    axes[1, 0] = plt.subplot(2, 3, 4)
+    axes[1, 1] = plt.subplot(2, 3, 5)
+    axes[1, 2] = plt.subplot(2, 3, 6)
 
-# Display results
-fig = plt.figure(figsize=(8, 5))
-axes = np.zeros((2, 3), dtype=np.object)
-axes[0, 0] = plt.subplot(2, 3, 1)
-axes[0, 1] = plt.subplot(2, 3, 2, sharex=axes[0, 0], sharey=axes[0, 0])
-axes[0, 2] = plt.subplot(2, 3, 3, sharex=axes[0, 0], sharey=axes[0, 0])
-axes[1, 0] = plt.subplot(2, 3, 4)
-axes[1, 1] = plt.subplot(2, 3, 5)
-axes[1, 2] = plt.subplot(2, 3, 6)
+    ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
+    ax_img.set_title('Low contrast image')
 
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(img, axes[:, 0])
-ax_img.set_title('Low contrast image')
+    y_min, y_max = ax_hist.get_ylim()
+    ax_hist.set_ylabel('Number of pixels')
+    ax_hist.set_yticks(np.linspace(0, y_max, 5))
 
-y_min, y_max = ax_hist.get_ylim()
-ax_hist.set_ylabel('Number of pixels')
-ax_hist.set_yticks(np.linspace(0, y_max, 5))
+    ax_img, ax_hist, ax_cdf = plot_img_and_hist(gamma_corrected, axes[:, 1])
+    ax_img.set_title('Gamma correction')
 
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(gamma_corrected, axes[:, 1])
-ax_img.set_title('Gamma correction')
+    ax_img, ax_hist, ax_cdf = plot_img_and_hist(logarithmic_corrected, axes[:, 2])
+    ax_img.set_title('Logarithmic correction')
 
-ax_img, ax_hist, ax_cdf = plot_img_and_hist(logarithmic_corrected, axes[:, 2])
-ax_img.set_title('Logarithmic correction')
+    ax_cdf.set_ylabel('Fraction of total intensity')
+    ax_cdf.set_yticks(np.linspace(0, 1, 5))
 
-ax_cdf.set_ylabel('Fraction of total intensity')
-ax_cdf.set_yticks(np.linspace(0, 1, 5))
+    # prevent overlap of y-axis labels
+    fig.tight_layout()
 
-# prevent overlap of y-axis labels
-fig.tight_layout()
-plt.savefig(r'contrast/' + str(num) + '.png')
-gc.collect()
+    return plt, gamma_corrected, logarithmic_corrected
+
+
+def save(plot, name):
+    plot.savefig(r'contrast/' + str(name) + '.png')
+    gc.collect()
+
+
+if __name__ == '__main__':
+    num = 8
+    tmp_img = io.imread(r'pictures/' + str(num) + '.png')
+    p, gamma, logarithmic = main(tmp_img)
+    save(p, num)
