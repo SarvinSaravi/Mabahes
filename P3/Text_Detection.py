2bc4ebd30d18affd8e2889f8f7b41a8b3f9b5a6b
SarvinSaravi <sarvin2000@gmail.com>
2020-05-15 13:34:35 +0430

    S change result without spacing

---
+++ P3/Text_Detection.py
@@ -0,0 +1,36 @@
+import gc
+import matplotlib.pyplot as plt
+from skimage import io, color, img_as_float
+
+filepath = 'picture\\Q3.png'
+# image = io.imread(filepath, as_gray=True)
+image = io.imread(filepath)
+
+image_bw = img_as_float(image)
+image_bw = color.rgb2gray(image)
+
+rows = []
+cols = []
+
+for r in range(100):
+    for c in range(100):
+        if image_bw[r][c] == 0:
+            if r not in rows:
+                rows.append(r)
+            if c not in cols:
+                cols.append(c)
+
+row_x1 = min(rows)
+row_x2 = max(rows)
+col_y1 = min(cols)
+col_y2 = max(cols)
+
+image_bw[row_x1, col_y1:col_y2] = 0
+image_bw[row_x2, col_y1:col_y2] = 0
+image_bw[row_x1:row_x2, col_y1] = 0
+image_bw[row_x1:row_x2, col_y2] = 0
+
+plt.imshow(image_bw)
+
+plt.savefig(r'picture/result1.png')
+gc.collect()
