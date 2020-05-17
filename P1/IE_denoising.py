0bc5363adca43c9a1299a0b6718907b5bfae1fb8
SarvinSaravi <sarvin2000@gmail.com>
2020-05-17 12:58:27 +0430

    Sarvin changes -> denoising -> v2 modular version

--- P1/IE_denoising.py
+++ P1/IE_denoising.py
@@ -7,6 +7,10 @@ import numpy as np
 from skimage.util.dtype import dtype_range
 from skimage._shared.utils import warn, check_shape_equality
 
+# import sys
+# sys.path.insert(1, "IE_scaling")
+# import IE_scaling as my_module
+
 __all__ = ['mean_squared_error',
            'normalized_root_mse',
            'peak_signal_noise_ratio',
@@ -68,80 +72,95 @@ def peak_signal_noise_ratio(image_true, image_test, *, data_range=None):
     return 10 * np.log10((data_range ** 2) / err)
 
 
-# change this number to access different pictures
-num = 10
+def main(image, noise):
+    my_picture = image
 
-# Load an example image
-img = io.imread('pictures\\' + str(num) + '.png')
-img = color.rgb2gray(img)
+    sigma = noise
+    noisy = random_noise(my_picture, var=sigma ** 2)
 
-my_picture = img_as_float(img)
-my_picture = my_picture[2000:2500, 2000:2500]
+    # estimate the noise standard deviation from the noisy image
+    sigma_est = np.mean(estimate_sigma(noisy, multichannel=True))
+    print(f"estimated noise standard deviation = {sigma_est}")
 
-sigma = 0.05
-noisy = random_noise(my_picture, var=sigma ** 2)
+    patch_kw = dict(patch_size=5,  # 5x5 patches
+                    patch_distance=6,  # 13x13 search area
+                    multichannel=True)
 
-# estimate the noise standard deviation from the noisy image
-sigma_est = np.mean(estimate_sigma(noisy, multichannel=True))
-print(f"estimated noise standard deviation = {sigma_est}")
+    # slow algorithm
+    denoise = denoise_nl_means(noisy, h=1.15 * sigma_est, fast_mode=False,
+                               **patch_kw)
 
-patch_kw = dict(patch_size=5,  # 5x5 patches
-                patch_distance=6,  # 13x13 search area
-                multichannel=True)
+    # slow algorithm, sigma provided
+    denoise2 = denoise_nl_means(noisy, h=0.8 * sigma_est, sigma=sigma_est,
+                                fast_mode=False, **patch_kw)
 
-# slow algorithm
-denoise = denoise_nl_means(noisy, h=1.15 * sigma_est, fast_mode=False,
-                           **patch_kw)
+    # fast algorithm
+    denoise_fast = denoise_nl_means(noisy, h=0.8 * sigma_est, fast_mode=True,
+                                    **patch_kw)
 
-# slow algorithm, sigma provided
-denoise2 = denoise_nl_means(noisy, h=0.8 * sigma_est, sigma=sigma_est,
-                            fast_mode=False, **patch_kw)
+    # fast algorithm, sigma provided
+    denoise2_fast = denoise_nl_means(noisy, h=0.6 * sigma_est, sigma=sigma_est,
+                                     fast_mode=True, **patch_kw)
 
-# fast algorithm
-denoise_fast = denoise_nl_means(noisy, h=0.8 * sigma_est, fast_mode=True,
-                                **patch_kw)
+    fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(8, 6),
+                           sharex=True, sharey=True)
 
-# fast algorithm, sigma provided
-denoise2_fast = denoise_nl_means(noisy, h=0.6 * sigma_est, sigma=sigma_est,
-                                 fast_mode=True, **patch_kw)
+    ax[0, 0].imshow(noisy, cmap=plt.cm.gray)
+    ax[0, 0].axis('off')
+    ax[0, 0].set_title('noisy')
+    ax[0, 1].imshow(denoise, cmap=plt.cm.gray)
+    ax[0, 1].axis('off')
+    ax[0, 1].set_title('non-local means\n(slow)')
+    ax[0, 2].imshow(denoise2, cmap=plt.cm.gray)
+    ax[0, 2].axis('off')
+    ax[0, 2].set_title('non-local means\n(slow, using $\\sigma_{est}$)')
+    ax[1, 0].imshow(my_picture, cmap=plt.cm.gray)
+    ax[1, 0].axis('off')
+    ax[1, 0].set_title('original\n(noise free)')
+    ax[1, 1].imshow(denoise_fast, cmap=plt.cm.gray)
+    ax[1, 1].axis('off')
+    ax[1, 1].set_title('non-local means\n(fast)')
+    ax[1, 2].imshow(denoise2_fast, cmap=plt.cm.gray)
+    ax[1, 2].axis('off')
+    ax[1, 2].set_title('non-local means\n(fast, using $\\sigma_{est}$)')
 
-fig, ax = plt.subplots(nrows=2, ncols=3, figsize=(8, 6),
-                       sharex=True, sharey=True)
+    fig.tight_layout()
 
-ax[0, 0].imshow(noisy)
-ax[0, 0].axis('off')
-ax[0, 0].set_title('noisy')
-ax[0, 1].imshow(denoise)
-ax[0, 1].axis('off')
-ax[0, 1].set_title('non-local means\n(slow)')
-ax[0, 2].imshow(denoise2)
-ax[0, 2].axis('off')
-ax[0, 2].set_title('non-local means\n(slow, using $\\sigma_{est}$)')
-ax[1, 0].imshow(my_picture)
-ax[1, 0].axis('off')
-ax[1, 0].set_title('original\n(noise free)')
-ax[1, 1].imshow(denoise_fast)
-ax[1, 1].axis('off')
-ax[1, 1].set_title('non-local means\n(fast)')
-ax[1, 2].imshow(denoise2_fast)
-ax[1, 2].axis('off')
-ax[1, 2].set_title('non-local means\n(fast, using $\\sigma_{est}$)')
+    # print PSNR metric for each case
+    psnr_noisy = peak_signal_noise_ratio(my_picture, noisy)
+    psnr = peak_signal_noise_ratio(my_picture, denoise)
+    psnr2 = peak_signal_noise_ratio(my_picture, denoise2)
+    psnr_fast = peak_signal_noise_ratio(my_picture, denoise_fast)
+    psnr2_fast = peak_signal_noise_ratio(my_picture, denoise2_fast)
 
-fig.tight_layout()
+    print(f"PSNR (noisy) = {psnr_noisy:0.2f}")
+    print(f"PSNR (slow) = {psnr:0.2f}")
+    print(f"PSNR (slow, using sigma) = {psnr2:0.2f}")
+    print(f"PSNR (fast) = {psnr_fast:0.2f}")
+    print(f"PSNR (fast, using sigma) = {psnr2_fast:0.2f}")
 
-# print PSNR metric for each case
-psnr_noisy = peak_signal_noise_ratio(my_picture, noisy)
-psnr = peak_signal_noise_ratio(my_picture, denoise)
-psnr2 = peak_signal_noise_ratio(my_picture, denoise2)
-psnr_fast = peak_signal_noise_ratio(my_picture, denoise_fast)
-psnr2_fast = peak_signal_noise_ratio(my_picture, denoise2_fast)
+    return plt, denoise, denoise2, denoise_fast, denoise2_fast, noisy
 
-print(f"PSNR (noisy) = {psnr_noisy:0.2f}")
-print(f"PSNR (slow) = {psnr:0.2f}")
-print(f"PSNR (slow, using sigma) = {psnr2:0.2f}")
-print(f"PSNR (fast) = {psnr_fast:0.2f}")
-print(f"PSNR (fast, using sigma) = {psnr2_fast:0.2f}")
 
-# save the result
-plt.savefig(r'denoising/' + str(num) + '.png')
-gc.collect()
+def save(plot, name):
+    plot.savefig(r'denoising/' + str(name) + '.png')
+    gc.collect()
+
+
+if __name__ == '__main__':
+    # change this number to access different pictures
+    num = 3
+
+    # Load an example image
+    img = io.imread('pictures\\' + str(num) + '.png')
+
+    # process on image
+    img = img_as_float(img)
+    img = color.rgb2gray(img)
+    print(img.shape)
+    # plt_v, img = my_module.main(img, 0.2)
+    img = img[2100:2700, 2900:3500]
+    print(img.shape)
+
+    p, dn, dn2, dn_f, dn_f2, nsy = main(img, 0.1)
+    save(p, num)
